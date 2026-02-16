adfffbac53180ad8753754fe4bf144e0f98ceb5a
Raphael Muniz <raphaelmunizvarela@gmail.com>
2026-02-16 03:24:59 -0300

    fix

--- src/test/java/io/github/raphaelmun1z/config/TestConfig.java
+++ src/test/java/io/github/raphaelmun1z/config/TestConfig.java
@@ -1,6 +1,6 @@
 package io.github.raphaelmun1z.config;
 
 public class TestConfig {
-    public static final int SERVER_PORT = 8888;
+    public static final int SERVER_PORT = 8080;
     public static final String CONTENT_TYPE_JSON = "application/json";
 }
